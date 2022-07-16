1be4c6f4f62c978edb38a7d3b6441322ee001e79
Rayan Kazi <rayankazi7515@gmail.com>
2022-07-15 19:33:09 -0700

    fixed avatar component in subreddit row

--- src/components/SubredditRow.tsx
+++ src/components/SubredditRow.tsx
@@ -17,7 +17,7 @@ const SubredditRow: NextComponentType<
     <div className="flex items-center space-x-2 border-t bg-white px-4 py-2 last:rounded-b">
       <p>{index + 1}</p>
       <ChevronUpIcon className="h-4 w-4 flex-shrink-0 text-green-400" />
-      <Avatar seed={`/subreddit/${topic}`} />
+      <Avatar seed={topic} />
       <p className="flex-1 truncate">r/{topic}</p>
 
       <Link href={`/subreddit/${topic}`}>
